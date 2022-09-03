7769efa19a5ccb8b8c19cb8817fc005b5147ef91
apsamuel <aaron.psamuel@gmail.com>
2022-09-03 14:52:54 -0400

    feat: init tools

---
+++ src/lib/tools/Scaling.js
@@ -0,0 +1,47 @@
+export function constantScaler(bodies = []) {
+  return []
+}
+
+export function linearScaler(bodies = []) {
+  return []
+}
+
+export function massScaler(bodies = []) {
+  return []
+}
+
+export function volumeScaler(bodies = []) {
+  return []
+}
+
+export function radiusScaler(bodies = []) {
+  return []
+}
+
+export function orbitScaler(bodies = []) {
+  return []
+}
+
+export function planetScaler(bodies = []) {
+  return []
+}
+
+export function moonScaler(bodies = []) {
+  return []
+}
+
+export function dwarfScaler(bodies = []) {
+  return []
+}
+
+export function asteroidScaler(bodies = []) {
+  return []
+}
+
+export function cometScaler(bodies = []) {
+  return []
+}
+
+export function starScaler(bodies = []) {
+  return []
+}
