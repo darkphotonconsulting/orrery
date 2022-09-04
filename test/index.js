afe7f121c143ae849edd23371613817665353b80
apsamuel <aaron.psamuel@gmail.com>
2022-09-04 12:51:16 -0400

    test: initialize tests

---
+++ test/index.js
@@ -0,0 +1,13 @@
+import { CelestialBodies } from '../src/lib/data/CelestialBodies.js'
+import { CelestialBodyScaler } from '../src/lib/tools/Scalers.js'
+
+const celestials = new CelestialBodies()
+const planets = await celestials.planets()
+const scaler = new CelestialBodyScaler()
+console.log(
+  scaler.linearTransformation({
+    bodies: planets,
+    rangeMinimum: 0,
+    rangeMaximum: 1000,
+  })
+)
