2f3bb1d2749851c01bea33b421b27788738a5bd7
apsamuel <aaron.psamuel@gmail.com>
2022-09-10 10:27:08 -0400

    chore: playing with uv functions

--- src/lib/tools/Helpers.js
+++ src/lib/tools/Helpers.js
@@ -30,34 +30,37 @@ export const circlePoints = ({
   return points
 }
 // (x,y,z) = (r*(cos(theta)*sin(phi)) , r*(sin(theta)*sin(phi)) , r*(cos(phi)) )
+
+
+const uv = ({
+  step
+}) => {
+  const ustep = (Math.PI) / step
+  const vstep = (2 * Math.PI) / step
+  const umax = Math.PI
+  const vmax = 2 * Math.PI
+  const vsteps = []
+  const usteps = []
+  for (let v = 0; v < vmax; v += vstep) {
+    vsteps.push(v)
+  }
+  for (let u = 0; u < umax; u += ustep) {
+    usteps.push(u)
+  }
+  // return {
+  //   u: usteps,
+  //   v: vsteps
+  // }
+  return usteps.map(u => vsteps.map(v => [u, v]))
+  .flat()
+}
+
 export const spherePoints = ({
   radius = 10,
   step = 32,
-
 }) => {
-  const points = []
-  // const steps = Array.from(Array(step)).keys()
-  let u = 0
-  let v = 0
-  // for (const step of steps) {
-  //   u += step
-  //   v += step
-  //   const x = radius * (Math.cos(u) * Math.sin(v))
-  //   const y = radius * (Math.sin(u) * Math.sin(v))
-  //   const z = radius * (Math.cos(v))
-  //   points.push(new THREE.Vector3(x, y, z))
-  // }
-
-  while (u <= Math.PI && v <= 2 * Math.PI) {
-    u += step
-    v += step
-    const x = radius * (Math.cos(u) * Math.sin(v))
-    const y = radius * (Math.sin(u) * Math.sin(v))
-    const z = radius * (Math.cos(v))
-    points.push(new THREE.Vector3(x, y, z))
-  }
-
-  return points
-
+  const steps = uv({step})
+  // const { u, v} = steps
+  return steps
 
 }
