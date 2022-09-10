b4fa2804b7b11f0bff22cba3f4cb15e83a26f7bd
apsamuel <aaron.psamuel@gmail.com>
2022-09-10 08:28:53 -0400

    chore: refresh orbital controls

--- src/lib/components/scene/Navigation.jsx
+++ src/lib/components/scene/Navigation.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
-
+import { extend, useThree } from '@react-three/fiber';
 import {
   OrbitControls,
 } from '@react-three/drei';
 
+extend({OrbitControls})
+
 export function Navigation({pan = true, zoom = true, rotate = true, ...props}) {
   const ref = React.useRef()
+  const { camera, gl: { domElement } } = useThree();
   return (
     <OrbitControls
       ref={ref}
       enablePan={pan}
       enableZoom={zoom}
       enableRotate={rotate}
+      args={[camera, domElement]}
     />
   )
 }
