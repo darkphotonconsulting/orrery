1b7875a15cb924ff9c8b5430ecf5adc9eca2ce32
apsamuel <aaron.psamuel@gmail.com>
2022-09-10 14:48:39 -0400

    feat: adds StarGroup

---
+++ src/lib/components/objects/StarGroup.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import {
+  useFrame,
+  // useLoader,
+  // useThree,
+}  from '@react-three/fiber';
+
+import {
+  Star
+} from './Star.jsx';
+
+export function StarGroup({
+  stars = [],
+  planets = [],
+  ...props
+}) {
+  const groupRef = React.useRef()
+  useFrame((state, delta) => {
+  })
+
+  const renderedStars = stars.map((star, index) => {
+    return (
+      <group>
+        <Star
+          scale={[1.0, 1.0, 1.0]}
+          key={`star-body-${star.englishName.toLowerCase()}`}
+          meshPositionX={0}
+          meshPositionY={0}
+          meshPositionZ={0}
+          meshRotationX={0}
+          meshRotationY={0}
+          meshRotationZ={0}
+          useAmbientLight={true}
+          useSpotLight={false}
+          spotlightPositionX={0}
+          spotlightPositionY={0}
+          spotlightPositionZ={0}
+          spotlightAngle={0.3}
+          lightIntensity={0.5}
+          widthSections={30}
+          heightSections={30}
+          wireFrame={false}
+          baseColor={'green'}
+          radius={star.equaRadius}
+          index={0}
+          userData={{
+            star,
+          }}
+        />
+      </group>
+    )
+  })
+
+  return (
+    <group ref={groupRef}>
+      {renderedStars}
+    </group>
+  )
+}
