8589ac804d83ad184f0681ead5a9b0487836d0fa
apsamuel <aaron.psamuel@gmail.com>
2022-10-02 16:18:43 -0400

    chore: allow easy debugging of orbitalControls

--- src/lib/components/scene/meta/Navigation.jsx
+++ src/lib/components/scene/meta/Navigation.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
-import { extend, useThree } from '@react-three/fiber';
+import { extend, useThree, useFrame } from '@react-three/fiber';
 import {
   OrbitControls,
 } from '@react-three/drei';
+
 
 extend({OrbitControls})
 
 export function Navigation({pan = true, zoom = true, rotate = true, ...props}) {
   const ref = React.useRef()
   const { camera, gl: { domElement } } = useThree();
+  useFrame((state) => {
+    // console.log(state)
+  })
+  React.useEffect(() => {
+    if (!!ref.current) {
+      // console.log(ref.current)
+    }
+  }, [ref.current])
   return (
     <OrbitControls
       ref={ref}
