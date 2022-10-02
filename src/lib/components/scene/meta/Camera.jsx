5dad041fb62901491e9fd3876b250b755e78acef
apsamuel <aaron.psamuel@gmail.com>
2022-10-02 16:16:38 -0400

    chore: use camera ref

--- src/lib/components/scene/meta/Camera.jsx
+++ src/lib/components/scene/meta/Camera.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { PerspectiveCamera } from '@react-three/drei';
+// import { extend, useThree, useFrame } from '@react-three/fiber';
 import {
   useThree,
   useFrame
@@ -7,19 +8,29 @@ import {
 
 
 export function Camera({ fov = 75, position = [10,15,10], ...props}) {
-  const ref = React.useRef()
-  const { camera, gl: { domElement } } = useThree();
+  const cameraRef = React.useRef()
+  const {
+    camera,
+    // gl: { domElement }
+  } = useThree();
   useFrame(({ clock }) => {
     camera.fov = fov
+    console.log(cameraRef)
   })
 
+
+
+  // console.log(PerspectiveCamera)
   return (
     <group>
       {/* <cameraHelper ref={ref} args={[camera, domElement]}/> */}
       <PerspectiveCamera
+        ref={cameraRef}
         makeDefault
         position={position}
         fov={fov}
+        far={5000}
+        zoom={2}
       />
     </group>
 
