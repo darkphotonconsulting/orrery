86049945c363d550bc865dfc4c2798be3d368a85
apsamuel <aaron.psamuel@gmail.com>
2022-10-02 16:25:31 -0400

    chore: enhancement data

---
+++ src/lib/data/SolarSystemDetails.js
@@ -0,0 +1,38 @@
+export const solarSystemDetails = {
+  planets: {
+    mercury: {
+      type: 'terrestrial',
+
+    },
+    venus: {
+      type: 'terrestrial',
+
+    },
+    earth: {
+      type: 'terrestrial',
+
+    },
+    mars: {
+      type: 'terrestrial',
+
+    },
+    jupiter: {
+      type: 'gas',
+
+    },
+    saturn: {
+      type: 'gas'
+    },
+    uranus: {
+      type: 'gas'
+    },
+    neptune: {
+      type: 'gas'
+    }
+  },
+  dwarfs: {
+    pluto: {
+      type: 'terrestrial'
+    }
+  }
+}
