342b452aea1bfc011129095310a1f34c418bd331
apsamuel <aaron.psamuel@gmail.com>
2022-10-07 17:05:01 -0400

    chore: add mission success status

--- src/lib/data/EnhancementData.js
+++ src/lib/data/EnhancementData.js
@@ -77,7 +77,8 @@ export const enhancementData = {
           `,
           year: 1960,
           image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Sunspots_2001-2009.jpg/800px-Sunspots_2001-2009.jpg',
-          groups: ['NASA']
+          groups: ['NASA'],
+          success: true,
         },
         {
           name: 'Pioneer 6',
@@ -86,7 +87,8 @@ export const enhancementData = {
           `,
           year: 1965,
           image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Sunspots_2001-2009.jpg/800px-Sunspots_2001-2009.jpg',
-          groups: ['NASA']
+          groups: ['NASA'],
+          success: true,
         },
         {
           name: 'Pioneer 7',
@@ -95,7 +97,8 @@ export const enhancementData = {
           `,
           year: 1966,
           image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Sunspots_2001-2009.jpg/800px-Sunspots_2001-2009.jpg',
-          groups: ['NASA']
+          groups: ['NASA'],
+          success: true,
         },
         {
           name: 'Pioneer 8',
@@ -104,7 +107,8 @@ export const enhancementData = {
           `,
           year: 1967,
           image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Sunspots_2001-2009.jpg/800px-Sunspots_2001-2009.jpg',
-          groups: ['NASA']
+          groups: ['NASA'],
+          success: true,
         },
         {
           name: 'Pioneer 9',
@@ -113,7 +117,8 @@ export const enhancementData = {
           `,
           year: 1968,
           image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Sunspots_2001-2009.jpg/800px-Sunspots_2001-2009.jpg',
-          groups: ['NASA']
+          groups: ['NASA'],
+          success: true,
         },
         {
           name: 'Skylab Apollo Solar Observatory',
@@ -122,7 +127,8 @@ export const enhancementData = {
           `,
           year: 1973,
           image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Sunspots_2001-2009.jpg/800px-Sunspots_2001-2009.jpg',
-          groups: ['NASA']
+          groups: ['NASA'],
+          success: true,
         },
         {
           name: 'Helios A',
@@ -131,7 +137,8 @@ export const enhancementData = {
           `,
           year: 1974,
           image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Sunspots_2001-2009.jpg/800px-Sunspots_2001-2009.jpg',
-          groups: ['NASA', 'German Aerospace Center']
+          groups: ['NASA', 'German Aerospace Center'],
+          success: true,
         },
         {
           name: 'Helios B',
@@ -140,7 +147,8 @@ export const enhancementData = {
           `,
           year: 1976,
           image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Sunspots_2001-2009.jpg/800px-Sunspots_2001-2009.jpg',
-          groups: ['NASA', 'German Aerospace Center']
+          groups: ['NASA', 'German Aerospace Center'],
+          success: true,
         },
         {
           name: 'Solar Maximum Mission',
@@ -149,7 +157,8 @@ export const enhancementData = {
           `,
           year: 1980,
           image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Sunspots_2001-2009.jpg/800px-Sunspots_2001-2009.jpg',
-          groups: ['NASA']
+          groups: ['NASA'],
+          success: true,
         },
         {
           name: 'Ulysses',
@@ -158,7 +167,8 @@ export const enhancementData = {
           `,
           year: 1990,
           image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Sunspots_2001-2009.jpg/800px-Sunspots_2001-2009.jpg',
-          groups: ['NASA', 'European Space Agency']
+          groups: ['NASA', 'European Space Agency'],
+          success: true,
         },
         {
           name: 'Yohkoh',
@@ -167,7 +177,8 @@ export const enhancementData = {
           `,
           year: 1991,
           image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Sunspots_2001-2009.jpg/800px-Sunspots_2001-2009.jpg',
-          groups: ['Japanese Aerospace Exploration Agency']
+          groups: ['Japanese Aerospace Exploration Agency'],
+          success: true,
         },
         {
           name: 'Solar and Heliospheric Observatory (SOHO)',
@@ -176,7 +187,8 @@ export const enhancementData = {
           `,
           year: 1995,
           image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Sunspots_2001-2009.jpg/800px-Sunspots_2001-2009.jpg',
-          groups: ['NASA', 'European Space Agency']
+          groups: ['NASA', 'European Space Agency'],
+          success: true,
         },
         {
           name: 'Genesis',
@@ -185,7 +197,8 @@ export const enhancementData = {
           `,
           year: 2001,
           image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Sunspots_2001-2009.jpg/800px-Sunspots_2001-2009.jpg',
-          groups: ['NASA']
+          groups: ['NASA'],
+          success: true,
         },
         {
           name: 'STEREO A',
@@ -194,7 +207,8 @@ export const enhancementData = {
           `,
           year: 2006,
           image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Sunspots_2001-2009.jpg/800px-Sunspots_2001-2009.jpg',
-          groups: ['NASA']
+          groups: ['NASA'],
+          success: true,
         },
         {
           name: 'STEREO B',
@@ -203,7 +217,8 @@ export const enhancementData = {
           `,
           year: 2006,
           image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Sunspots_2001-2009.jpg/800px-Sunspots_2001-2009.jpg',
-          groups: ['NASA']
+          groups: ['NASA'],
+          success: true,
         },
         {
           name: 'Hinode',
@@ -212,7 +227,8 @@ export const enhancementData = {
           `,
           year: 2006,
           image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Sunspots_2001-2009.jpg/800px-Sunspots_2001-2009.jpg',
-          groups: ['Japanese Aerospace Exploration Agency']
+          groups: ['Japanese Aerospace Exploration Agency'],
+          success: true,
         },
         {
           name: 'Solar Dynamics Observatory',
@@ -221,7 +237,8 @@ export const enhancementData = {
           `,
           year: 2010,
           image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Sunspots_2001-2009.jpg/800px-Sunspots_2001-2009.jpg',
-          groups: ['NASA']
+          groups: ['NASA'],
+          success: true,
         }
 
       ],
@@ -239,216 +256,10 @@ export const enhancementData = {
         corona: {},
       },
       features: [
-        {
-          name: 'sunspots',
-          description: `
-            Sunspots are temporary phenomena on the Sun's photosphere that appear as spots darker than the surrounding areas.
-            They are regions of reduced surface temperature caused by concentrations of magnetic field flux that inhibit convection.
-            The number of sunspots varies according to the approximately 11-year solar cycle.
-          `,
-          image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Sunspots_2001-2009.jpg/800px-Sunspots_2001-2009.jpg',
-        },
-        {
-          name: 'prominences',
-          description: `
-            Prominences are large, cool, gaseous structures suspended above the Sun's surface.
-            They are thought to be the result of magnetic fields that connect the Sun's surface to its atmosphere.
-            Prominences are often associated with solar flares and coronal mass ejections.
-          `,
-          image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Sunspots_2001-2009.jpg/800px-Sunspots_2001-2009.jpg',
-        },
-        {
-          name: 'granulation',
-          description: `
-            Granulation is the pattern of dark and light spots on the Sun's photosphere.
-          `,
-          image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Sunspots_2001-2009.jpg/800px-Sunspots_2001-2009.jpg',
-        },
-        {
-          name: 'spicules',
-          description: `
-            Spicules are small, hot, gaseous structures that extend from the Sun's surface into the solar atmosphere.
-          `,
-          image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Sunspots_2001-2009.jpg/800px-Sunspots_2001-2009.jpg',
-        },
-        {
-          name: 'filaments',
-          description: `
-            Filaments are long, cool, gaseous structures suspended above the Sun's surface.
-          `,
-          image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Sunspots_2001-2009.jpg/800px-Sunspots_2001-2009.jpg',
-        },
-        {
-          name: 'coronal mass ejections',
-          description: `
-            Coronal mass ejections (CMEs) are large-scale eruptions of plasma and magnetic fields from the Sun's corona,
-          `,
-          image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Sunspots_2001-2009.jpg/800px-Sunspots_2001-2009.jpg',
-        },
-        {
-          name: 'solar flares',
-          description: `
-            Solar flares are sudden, intense bursts of radiation from the Sun's surface.
-          `,
-          image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Sunspots_2001-2009.jpg/800px-Sunspots_2001-2009.jpg',
-        }
+
       ],
       missions: [
-        {
-          name: 'Pioneer 5',
-          description: `
-            Pioneer 5 was the first spacecraft to reach the vicinity of the Sun.
-            It was launched on 5 August 1958, and reached the Sun on 5 September 1958.
-            Lack of a camera prevented it from taking any pictures of the Sun.
-          `,
-          year: 1960,
-          image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Sunspots_2001-2009.jpg/800px-Sunspots_2001-2009.jpg',
-          groups: ['NASA']
-        },
-        {
-          name: 'Pioneer 6',
-          description: `
-            Pioneer 6 was the second spacecraft to reach the vicinity of the Sun.
-          `,
-          year: 1965,
-          image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Sunspots_2001-2009.jpg/800px-Sunspots_2001-2009.jpg',
-          groups: ['NASA']
-        },
-        {
-          name: 'Pioneer 7',
-          description: `
-            Pioneer 7 was the third spacecraft to reach the vicinity of the Sun.
-          `,
-          year: 1966,
-          image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Sunspots_2001-2009.jpg/800px-Sunspots_2001-2009.jpg',
-          groups: ['NASA']
-        },
-        {
-          name: 'Pioneer 8',
-          description: `
-            Pioneer 8 was the fourth spacecraft to reach the vicinity of the Sun.
-          `,
-          year: 1967,
-          image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Sunspots_2001-2009.jpg/800px-Sunspots_2001-2009.jpg',
-          groups: ['NASA']
-        },
-        {
-          name: 'Pioneer 9',
-          description: `
-            Pioneer 9 was the fifth spacecraft to reach the vicinity of the Sun.
-          `,
-          year: 1968,
-          image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Sunspots_2001-2009.jpg/800px-Sunspots_2001-2009.jpg',
-          groups: ['NASA']
-        },
-        {
-          name: 'Skylab Apollo Solar Observatory',
-          description: `
-            Skylab Apollo Solar Observatory was a solar observatory that was launched on 14 May 1973.
-          `,
-          year: 1973,
-          image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Sunspots_2001-2009.jpg/800px-Sunspots_2001-2009.jpg',
-          groups: ['NASA']
-        },
-        {
-          name: 'Helios A',
-          description: `
-            Helios A was the first of two spacecraft that were launched to study the Sun.
-          `,
-          year: 1974,
-          image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Sunspots_2001-2009.jpg/800px-Sunspots_2001-2009.jpg',
-          groups: ['NASA', 'German Aerospace Center']
-        },
-        {
-          name: 'Helios B',
-          description: `
-            Helios B was the second of two spacecraft that were launched to study the Sun.
-          `,
-          year: 1976,
-          image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Sunspots_2001-2009.jpg/800px-Sunspots_2001-2009.jpg',
-          groups: ['NASA', 'German Aerospace Center']
-        },
-        {
-          name: 'Solar Maximum Mission',
-          description: `
-            Solar Maximum Mission was a NASA mission that was launched on 8 November 1980.
-          `,
-          year: 1980,
-          image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Sunspots_2001-2009.jpg/800px-Sunspots_2001-2009.jpg',
-          groups: ['NASA']
-        },
-        {
-          name: 'Ulysses',
-          description: `
-            Ulysses was a NASA mission that was launched on 18 October 1990.
-          `,
-          year: 1990,
-          image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Sunspots_2001-2009.jpg/800px-Sunspots_2001-2009.jpg',
-          groups: ['NASA', 'European Space Agency']
-        },
-        {
-          name: 'Yohkoh',
-          description: `
-            Yohkoh was a Japanese mission that was launched on 14 February 1991.
-          `,
-          year: 1991,
-          image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Sunspots_2001-2009.jpg/800px-Sunspots_2001-2009.jpg',
-          groups: ['Japanese Aerospace Exploration Agency']
-        },
-        {
-          name: 'Solar and Heliospheric Observatory (SOHO)',
-          description: `
-            Solar and Heliospheric Observatory (SOHO) was a NASA mission that was launched on 2 December 1995.
-          `,
-          year: 1995,
-          image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Sunspots_2001-2009.jpg/800px-Sunspots_2001-2009.jpg',
-          groups: ['NASA', 'European Space Agency']
-        },
-        {
-          name: 'Genesis',
-          description: `
-            Genesis was a NASA mission that was launched on 8 September 2001.
-          `,
-          year: 2001,
-          image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Sunspots_2001-2009.jpg/800px-Sunspots_2001-2009.jpg',
-          groups: ['NASA']
-        },
-        {
-          name: 'STEREO A',
-          description: `
-            STEREO A was a NASA mission that was launched on 25 October 2006.
-          `,
-          year: 2006,
-          image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Sunspots_2001-2009.jpg/800px-Sunspots_2001-2009.jpg',
-          groups: ['NASA']
-        },
-        {
-          name: 'STEREO B',
-          description: `
-            STEREO B was a NASA mission that was launched on 25 October 2006.
-          `,
-          year: 2006,
-          image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Sunspots_2001-2009.jpg/800px-Sunspots_2001-2009.jpg',
-          groups: ['NASA']
-        },
-        {
-          name: 'Hinode',
-          description: `
-            Hinode was a Japanese mission that was launched on 22 September 2006.
-          `,
-          year: 2006,
-          image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Sunspots_2001-2009.jpg/800px-Sunspots_2001-2009.jpg',
-          groups: ['Japanese Aerospace Exploration Agency']
-        },
-        {
-          name: 'Solar Dynamics Observatory',
-          description: `
-            Solar Dynamics Observatory was a NASA mission that was launched on 11 February 2010.
-          `,
-          year: 2010,
-          image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Sunspots_2001-2009.jpg/800px-Sunspots_2001-2009.jpg',
-          groups: ['NASA']
-        }
+
 
       ],
     }
